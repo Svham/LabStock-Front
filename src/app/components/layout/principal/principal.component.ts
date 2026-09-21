85a9a4864c2c139dfefe440ba8db86d3bcebba4a
erickbarreto <erickfsbarreto@gmail.com>
2026-09-20 22:57:29 -0300

    removendo tab adicionar itens do header e corrigindo formatação do login

--- src/app/components/layout/principal/principal.component.ts
+++ src/app/components/layout/principal/principal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-principal',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
